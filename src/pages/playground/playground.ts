0e0591515687993cc35b2f85548348293c798a99
Alexey Eremin <a.eremin.msu@gmail.com>
2016-11-14 03:53:13 +0200

    Add example of external REST API polling (time API from http://davidayala.eu/current-time/ is used)

--- src/pages/playground/playground.ts
+++ src/pages/playground/playground.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 
 import { NavController } from 'ionic-angular';
 
@@ -7,9 +9,16 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'playground.html'
 })
 export class PlaygroundPage {
-
-  constructor(public navCtrl: NavController) {
-
+  currentTime: string;
+  constructor(public navCtrl: NavController,
+              private http: Http) {
+    
+    Observable.interval(10000).forEach((iter: number) => {
+      http.get('https://script.google.com/macros/s/AKfycbyd5AcbAnWi2Yn0xhFRbyzS4qMq1VucMVgVvhul5XqS9HkAyJY/exec').subscribe(
+        response => this.currentTime = response.json().hours + ':' + response.json().minutes + ':' + response.json().seconds,
+        error => console.error('Error: ' + error),
+        () => console.log('Completed!'));
+    });
   }
 
 }
