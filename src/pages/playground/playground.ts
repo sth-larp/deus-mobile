519ab9e796a2627c4ad9a728fa7de70f0f34eecb
Alexey Eremin <a.eremin.msu@gmail.com>
2016-11-14 23:38:09 +0200

    Add example button for "instant" data update

--- src/pages/playground/playground.ts
+++ src/pages/playground/playground.ts
@@ -13,12 +13,15 @@ export class PlaygroundPage {
   constructor(public navCtrl: NavController,
               private http: Http) {
     
-    Observable.interval(10000).forEach((iter: number) => {
-      http.get('https://script.google.com/macros/s/AKfycbyd5AcbAnWi2Yn0xhFRbyzS4qMq1VucMVgVvhul5XqS9HkAyJY/exec').subscribe(
+    Observable.interval(10000).forEach(() => this.enqueTimeUpdate());
+  }
+
+  enqueTimeUpdate() : void {
+      console.log('enqueTimeUpdate');
+      this.http.get('https://script.google.com/macros/s/AKfycbyd5AcbAnWi2Yn0xhFRbyzS4qMq1VucMVgVvhul5XqS9HkAyJY/exec').subscribe(
         response => this.currentTime = response.json().hours + ':' + response.json().minutes + ':' + response.json().seconds,
         error => console.error('Error: ' + error),
         () => console.log('Completed!'));
-    });
   }
 
 }
