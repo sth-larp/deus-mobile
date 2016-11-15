af595e6d6e26d16abe6c2e07c000cf2a6a82a956
Alexey Eremin <a.eremin.msu@gmail.com>
2016-11-16 01:24:36 +0200

    Fun with observables! Add example of NativeStorage usage and some dumb caching.

--- src/time/time.service.ts
+++ src/time/time.service.ts
@@ -2,14 +2,33 @@ import { Injectable } from '@angular/core'
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 
+import { NativeStorage } from 'ionic-native';
+
 @Injectable()
 export class TimeService {
     private _url: string = 'https://script.google.com/macros/s/AKfycbyd5AcbAnWi2Yn0xhFRbyzS4qMq1VucMVgVvhul5XqS9HkAyJY/exec';
     constructor(private _http: Http) {}
     
     getTime() : Observable<any> {
-        return this._http.get(this._url)
+        var observableHttp: Observable<any> = this._http.get(this._url)
             .map((response : Response) => response.json())
-            .do(json => console.log('Received json: ' + JSON.stringify(json)));
+            .do(json => console.log('Received json: ' + JSON.stringify(json)))
+            .do(json => this._cacheTime(json));
+        var observableLocal: Observable<any> = this._getCachedTime();
+        return Observable.concat(observableLocal, observableHttp);
     }
+
+    private _cacheTime(json : any) : void {
+        NativeStorage.setItem('current_time', json)
+            .then(
+                () => console.log('Stored item!'),
+                error => console.error('Error storing item', error)
+            );
+    }
+
+    private _getCachedTime() : Observable<any> {
+        return Observable.fromPromise(NativeStorage.getItem('current_time'));
+    }
+
 }
+
