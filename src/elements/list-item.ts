a9c95ba9e9202298591cbd589df4a109dc79d5e6
Alexey Eremin <a.eremin.msu@gmail.com>
2017-04-17 10:03:22 +0200

    Improve list page: support key-value case, support color change, support icons, make elements smaller (css magic!)

--- src/elements/list-item.ts
+++ src/elements/list-item.ts
@@ -3,7 +3,17 @@ import { Component, Input } from '@angular/core';
 export class ListItemData {
   public text: string;
   public subtext: string;
+  public value: string;
   public percent: number;
+  public progressBarColor: string;
+  public valueColor: string;
+
+  public hasIcon: boolean;
+  public icon: string;
+
+  public clickable: boolean;
+
+  public tag: string;
 }
 
 @Component({
