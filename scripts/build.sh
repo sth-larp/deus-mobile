a48e5efab4cf7b3d082580947a838fa7ecb73329
Alexey Eremin <a.eremin.msu@gmail.com>
2017-05-22 22:57:33 +0300

    Add hack from https://github.com/arnesson/cordova-plugin-firebase/issues/275#issuecomment-301894381 to hopefully fix https://deus2017.atlassian.net/projects/DEM/issues/DEM-80

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@ set -e
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then  
   ionic cordova platform add ios
+  cp GoogleService-Info.plist platforms/ios/deus-larp-2017/Resources/Resources
   ionic cordova build ios --device --buildConfig ./cordovaBuildConfig.json
 fi
 
