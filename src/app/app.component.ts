faebd03dc80d7a7889f90c241c41a3cb25ba3de4
Alexey Eremin <a.eremin.msu@gmail.com>
2017-06-16 21:41:49 +0200

    Remove Instabug again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,6 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { LoginPage } from '../pages/login';
 
-// Cordova has no typings available, hack to make it compile
-declare var cordova: any;
-
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
@@ -19,26 +16,6 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
-      cordova.plugins.instabug.activate(
-        {
-          android: 'c855fb2d481e82f46320fd94b20d94c3',
-          ios: '5546f72e8227ced7274417da0fdace8b'
-        },
-        'shake',
-        {
-          commentRequired: true,
-          colorTheme: 'dark',
-          shakingThresholdIPhone: '1.5',
-          shakingThresholdIPad: '0.6',
-          enableIntroDialog: false
-        },
-        () => {
-          console.log('Instabug initialized.');
-        },
-        (error) => {
-          console.error('Instabug could not be initialized - ' + error);
-        }
-      );
     });
   }
 }
