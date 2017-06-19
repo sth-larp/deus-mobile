7d82a30ecd3048594446aab138c999b18a8d1a92
Alexey Eremin <a.eremin.msu@gmail.com>
2017-06-19 21:32:11 +0300

    Add debug logging of iOS build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,8 @@ set -e
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then  
   ionic cordova platform add ios
   cp GoogleService-Info.plist platforms/ios/deus-larp-2017/Resources/Resources
+  ls  platforms/ios/deus-larp-2017/Resources/Resources/
+  cat platforms/ios/deus-larp-2017/Resources/Resources/*.plist
   ionic cordova build ios --device --buildConfig ./cordovaBuildConfig.json
 fi
 
