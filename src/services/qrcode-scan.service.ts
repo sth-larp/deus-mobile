c381fc795d3ab8db61fc6c69059019ca803ba786
Alexey Eremin <a.eremin.msu@gmail.com>
2017-06-21 23:57:16 +0200

    Provide better warning/error messages in case of failed QR code reading https://deus2017.atlassian.net/browse/DEM-132

--- src/services/qrcode-scan.service.ts
+++ src/services/qrcode-scan.service.ts
@@ -5,8 +5,11 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { LoggingService } from "./logging.service";
 import { CharacterPage } from "../pages/character";
 import { GeneralQRCodePage } from "../pages/general-qrcode";
-import { decode, QrData } from "deus-qr-lib"
+import { decode, QrData, FormatError } from "deus-qr-lib"
 import { MonotonicTimeService } from "./monotonic-time.service";
+
+class QrExpiredError extends Error {
+}
 
 @Injectable()
 export class QrCodeScanService {
@@ -38,7 +41,7 @@ export class QrCodeScanService {
         this._logging.info('QR code scanning was canncelled by user');
     }, (err) => {
       this._logging.warning('Error reading QR code: ' + err);
-      this.showInvalidQrWarning()
+      this.showCannotReadQrWarning();
     });
   }
 
@@ -47,20 +50,43 @@ export class QrCodeScanService {
       let data: QrData = decode(qr);
       this._logging.info('Decoded QR code: ' + JSON.stringify(data));
       if (data.validUntil < this._monotonicClock.getUnixTimeMs() / 1000)
-        throw Error('QR code expired');
+        throw new QrExpiredError('QR code expired');
       let modal = this._modalController.create(GeneralQRCodePage, { value: data });
       modal.present();
     } catch (e) {
       this._logging.warning('Unsupported QR code scanned, error: ' + e);
-      this.showInvalidQrWarning();
+      if (e instanceof QrExpiredError)
+        this.showExperidQrWarning()
+      else
+        this.showInvalidQrFormatWarning();
     }
   }
 
-  private showInvalidQrWarning() {
+  private showCannotReadQrWarning() {
     this._alertController.create({
-      title: 'Неподдерживаемый QR-код',
-      message: 'Приложение не может распознать QR-код. Если вы уверены, что это допустимый код ' +
-      'и вам точно необходимо его использовать, сфотографируйте код и отправьте эту фотографию с описанием ситуации на адрес support@alice.digital.',
+      title: 'Не получается отсканировать QR-код',
+      message: 'Приложение не может отсканировать QR-код. Пожалуйста, убедитесь, что у приложения есть ' +
+               'доступ к камере, QR код хорошего качества. Используйте кнопку включения подсветки при необходимости.',
+      buttons: ['Ок']
+    }).present();
+  }
+
+  private showInvalidQrFormatWarning() {
+    this._alertController.create({
+      title: 'Некорректный формат QR-кода',
+      message: 'QR-код распознан, но имеет неправильный формат. Если вы уверены, что это допустимый код ' +
+               'и вам точно необходимо его использовать, сфотографируйте код и отправьте эту фотографию ' +
+               'с описанием ситуации на адрес support@alice.digital.',
+      buttons: ['Ок']
+    }).present();
+  }
+
+  private showExperidQrWarning() {
+    this._alertController.create({
+      title: 'Срок действия QR-кода истек',
+      message: 'QR-код распознан, но срок его действия истек. Если вы уверены, что это допустимый код ' +
+               'и вам точно необходимо его использовать, сфотографируйте код и отправьте эту фотографию ' +
+               'с описанием ситуации на адрес support@alice.digital.',
       buttons: ['Ок']
     }).present();
   }
