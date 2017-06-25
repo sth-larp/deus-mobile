17d10fd41fb7c53a570764876310c1ce35544ed6
Andrei Matveiakin <a.matveyakin@gmail.com>
2017-06-25 16:51:55 +0200

    Remove spammy log messages

--- src/services/local-data.service.ts
+++ src/services/local-data.service.ts
@@ -11,15 +11,12 @@ export class LocalDataService {
 
   public toggleVr() {
     if (this._vrEnterTime == null) {
-      console.debug("### VR: in");
       this._vrEnterTime = this._time.getUnixTimeMs();
     } else {
-      console.debug("### VR: out");
       this._vrEnterTime = null;
     }
   }
   public vrEnterTime() : number {
-    console.debug("### get VR");
     return this._vrEnterTime;
   }
 }
