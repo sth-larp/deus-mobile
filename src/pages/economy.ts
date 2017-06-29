de2d49c524df96a9a8ef0bf0efec659326c4e97c
Alexey Eremin <a.eremin.msu@gmail.com>
2017-06-29 10:01:07 +0200

    Adjust UI of EconomyPage to match specification better

--- src/pages/economy.ts
+++ src/pages/economy.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { ListItemData } from "../elements/list-item";
+import { Http } from "@angular/http";
+import { AuthService } from "../services/auth.service";
+import { ModalController } from "ionic-angular";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'page-economy',
   templateUrl: 'economy.html'
 })
 export class EconomyPage {
-  public balance: ListItemData = {text: 'Баланс', value: "100500" };
+  public balance: ListItemData = { text: 'Баланс', value: "100500" };
 
-  constructor() {
+  public sendForm: FormGroup;
+  public receiveForm: FormGroup;
+
+  constructor(private _http: Http,
+    private _authService: AuthService,
+    private _modalController: ModalController,
+    private _formBuilder: FormBuilder) {
+
+    this.sendForm = this._formBuilder.group({
+      receiverId: ['', Validators.required],
+      amount: ['', Validators.required]
+    });
+
+    this.receiveForm = this._formBuilder.group({
+      amount: ['', Validators.required]
+    });
+  }
+
+  public sendMoney() {
+    console.warn("send");
+  }
+
+  public receiveMoney() {
+    console.warn("receive");
   }
 }
+
