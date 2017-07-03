2b073a5de96774071b177af175e42b8af1dfb340
Andrei Matveiakin <a.matveyakin@gmail.com>
2017-07-03 23:50:20 +0200

    Replace currency symbol with a simple abbreviation

--- src/services/economy.service.ts
+++ src/services/economy.service.ts
@@ -51,7 +51,7 @@ export class EconomyService {
       };
 
       const alert = this._alertCtrl.create({
-        message: `Перевести ${amount} ₵ на счет ${receiver}?`,
+        message: `Перевести ${amount} к. на счет ${receiver}?`,
         buttons: [
           {
             text: 'Отмена',
