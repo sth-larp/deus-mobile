f84732ba42641e41653305aae232692ef0a00f69
Andrei Matveiakin <a.matveyakin@gmail.com>
2017-07-08 21:28:14 +0200

    Refactor LocalDataService

--- src/services/local-data.service.ts
+++ src/services/local-data.service.ts
@@ -4,7 +4,7 @@ import { AuthService } from './auth.service';
 import { MonotonicTimeService } from './monotonic-time.service';
 import { NativeStorageService } from './native-storage.service';
 
-// TODO (Andrei): Store this data while one user stays logged in. No more, no less.
+// Permanent storage of per-account data
 @Injectable()
 export class LocalDataService {
   constructor(private _time: MonotonicTimeService,
@@ -12,33 +12,46 @@ export class LocalDataService {
               private _nativeStorage: NativeStorageService) {
   }
 
-  public async toggleVr() {
-    if (await this.inVr()) {
-      await this._nativeStorage.remove(this.key());
-    } else {
-      await this._nativeStorage.setItem(this.key(), this._time.getUnixTimeMs());
-    }
+  // General API
+  public setItem(key: string, value: any): Promise<any> {
+    return this._nativeStorage.setItem(this.makeGlobalKey(key), value);
   }
 
+  public getItem(key: string): Promise<any> {
+    return this._nativeStorage.getItem(this.makeGlobalKey(key));
+  }
+
+  public remove(key: string): Promise<any> {
+    return this._nativeStorage.remove(this.makeGlobalKey(key));
+  }
+
+  // VR API
+  public async toggleVr() {
+    if (await this.inVr()) {
+      await this.remove('vrEnterTime');
+    } else {
+      await this.setItem('vrEnterTime', this._time.getUnixTimeMs());
+    }
+  }
   public async inVr(): Promise<boolean> {
     try {
-      await this._nativeStorage.getItem(this.key());
+      await this.getItem('vrEnterTime');
       return true;
     } catch (e) {
       return false;
     }
   }
-
   public async secondsInVr(): Promise<number> {
     try {
-      const vrEnterTime = await this._nativeStorage.getItem(this.key());
+      const vrEnterTime = await this.getItem('vrEnterTime');
       return (this._time.getUnixTimeMs() - vrEnterTime) / 1000.;
     } catch (e) {
       return null;
     }
   }
 
-  private key(): string {
-    return this._authService.getUsername() + '/' + 'InVR';
+  // Helper functions
+  private makeGlobalKey(localKey: string): string {
+    return this._authService.getUsername() + '/' + localKey;
   }
 }
