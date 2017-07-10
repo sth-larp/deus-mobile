c8d16382fa7ad08275ebe636e10a7a8fdc601488
Alexey Eremin <a.eremin.msu@gmail.com>
2017-07-10 19:49:38 +0200

    Hopefully fix iOS travis build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 set -e
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then  
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   ionic cordova platform add ios --noresources
-  cp GoogleService-Info.plist platforms/ios/deus-larp-2017/Resources/Resources
+  cp GoogleService-Info.plist "platforms/ios/Deus LARP/Resources/Resources"
   ionic cordova build ios --device --buildConfig ./cordovaBuildConfig.json
-  ls  platforms/ios/deus-larp-2017/Resources/Resources/
-  echo "breakpoint"
-  ls  platforms/ios/deus-larp-2017/*
-  cat platforms/ios/deus-larp-2017/deus-larp-2017-Info.plist
 fi
 
-if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then  
+if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
   ionic cordova platform add android --noresources
   ionic cordova build android --device
 fi
--- scripts/upload-to-hockeyapp.sh
+++ scripts/upload-to-hockeyapp.sh
@@ -4,7 +4,7 @@ set -e
 changes=$(git log -1 --pretty=%B)
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  file_to_upload="platforms/ios/build/device/deus-larp-2017.ipa"
+  file_to_upload="platforms/ios/build/device/Deus LARP.ipa"
 fi
 
 if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
@@ -18,6 +18,6 @@ curl \
   -F "status=2" \
   -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN" \
   https://rink.hockeyapp.net/api/2/apps/upload
- 
-  
-  
+
+
+
