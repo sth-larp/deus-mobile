38aaeb99a4a86f7679286a9c605ea0a328e2a702
Alexey Eremin <a.eremin.msu@gmail.com>
2017-07-19 01:45:05 +0200

    Add --verbose to ionic command so Travis won't kill them because of inactivity

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,12 @@
 set -e
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  ionic cordova platform add ios --noresources --no-interactive
+  ionic cordova platform add ios --noresources --no-interactive --verbose
   cp GoogleService-Info.plist "platforms/ios/Deus LARP 2017/Resources/Resources"
-  ionic cordova build ios --device --buildConfig ./cordovaBuildConfig.json --no-interactive
+  ionic cordova build ios --device --buildConfig ./cordovaBuildConfig.json --no-interactive --verbose
 fi
 
 if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
-  ionic cordova platform add android --noresources --no-interactive
-  ionic cordova build android --device --no-interactive
+  ionic cordova platform add android --noresources --no-interactive --verbose
+  ionic cordova build android --device --no-interactive --verbose
 fi
