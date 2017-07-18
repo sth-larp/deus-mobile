77b158723e7a6d1163b04cd2f3234abe8d6f9466
Alexey Eremin <a.eremin.msu@gmail.com>
2017-07-19 01:18:23 +0200

    Run all ionic commands with --no-interactive on Travis

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,12 @@
 set -e
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  ionic cordova platform add ios --noresources
+  ionic cordova platform add ios --noresources --no-interactive
   cp GoogleService-Info.plist "platforms/ios/Deus LARP 2017/Resources/Resources"
-  ionic cordova build ios --device --buildConfig ./cordovaBuildConfig.json
+  ionic cordova build ios --device --buildConfig ./cordovaBuildConfig.json --no-interactive
 fi
 
 if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
-  ionic cordova platform add android --noresources
-  ionic cordova build android --device
+  ionic cordova platform add android --noresources --no-interactive
+  ionic cordova build android --device --no-interactive
 fi
