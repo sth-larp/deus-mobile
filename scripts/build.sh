f3251c2e899a0e52d3931691cbea111efa60108b
Alexey Eremin <aeremin@aeremin-macbookpro.roam.corp.google.com>
2017-07-19 13:00:25 +0200

    Use travis_wait to when adding ios platform to prevent Travis from killing the build

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  ionic cordova platform add ios --noresources --no-interactive --verbose
+  travis_wait 30 ionic cordova platform add ios --noresources --no-interactive --verbose
   cp GoogleService-Info.plist "platforms/ios/Deus LARP 2017/Resources/Resources"
   ionic cordova build ios --device --buildConfig ./cordovaBuildConfig.json --no-interactive --verbose
 fi
