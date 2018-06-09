9e0ecb53050771541019b3678a7bcbefefd462b6
Alexey Eremin <a.eremin.msu@mail.com>
2018-06-09 17:04:01 +0200

    Fix build

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -245,7 +245,7 @@ export class DataService implements ILoginListener {
       _id: this._authService.getUserId(),
       timestamp: this._time.getUnixTimeMs(),
       menu: { characterName: this._authService.getUserId() },
-      toolbar: { hitPoints: 9000, maxHitPoints: 9000 },
+      toolbar: { airSecondsLeft: 0 },
       passportScreen: { corporation: 'Ошибка', email: 'Ошибка', fullName: 'Ошибка', id: this._authService.getUserId() },
       pages: [
         errorPage,
