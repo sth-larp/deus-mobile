76b4ed23a5e21f214184a05b7bc3421363c20053
Alexey Eremin <a.eremin.msu@gmail.com>
2018-06-19 23:45:30 +0200

    Fix formatting

--- src/services/economy.service.ts
+++ src/services/economy.service.ts
@@ -14,8 +14,8 @@ interface EconomyData {
 @Injectable()
 export class EconomyService {
   constructor(private _authService: AuthService,
-    private _alertCtrl: EnhancedAlertController,
-    private _http: Http) { }
+              private _alertCtrl: EnhancedAlertController,
+              private _http: Http) { }
 
   public getEconomyData(): Promise<EconomyData> {
     return this._http.get(GlobalConfig.economyGetDataUrl + this._authService.getUserId(),
@@ -38,65 +38,65 @@ export class EconomyService {
       }).toPromise();
   }
 
-  public makeTransaction(receiver: string, amount: number, description: string): Promise < {} > {
-  // TODO: validate amount?
-  return new Promise((resolve, reject) => {
-    const notifyAndReject = (e: string) => {
+  public makeTransaction(receiver: string, amount: number, description: string): Promise<{}> {
+    // TODO: validate amount?
+    return new Promise((resolve, reject) => {
+      const notifyAndReject = (e: string) => {
+        this._alertCtrl.show({
+          title: 'Ошибка',
+          message: e,
+          buttons: [{ text: 'Ок', handler: reject }],
+        });
+      };
+
+      const notifySuccess = () => {
+        this._alertCtrl.show({
+          title: 'Успешно',
+          message: 'Перевод выполнен!',
+          buttons: [{ text: 'Ок', handler: resolve }],
+        });
+      };
+
+      let message = `Вы хотите перевести <b>${amount} кр.</b> на счет <b>${receiver}</b>?`;
+      if (description.length)
+        message = message + ` Назначение платежа: <b>${description}</b>.`;
+
       this._alertCtrl.show({
-        title: 'Ошибка',
-        message: e,
-        buttons: [{ text: 'Ок', handler: reject }],
-      });
-    };
-
-    const notifySuccess = () => {
-      this._alertCtrl.show({
-        title: 'Успешно',
-        message: 'Перевод выполнен!',
-        buttons: [{ text: 'Ок', handler: resolve }],
-      });
-    };
-
-    let message = `Вы хотите перевести <b>${amount} кр.</b> на счет <b>${receiver}</b>?`;
-    if (description.length)
-      message = message + ` Назначение платежа: <b>${description}</b>.`;
-
-    this._alertCtrl.show({
-      title: 'Подтверждение перевода',
-      message,
-      buttons: [
-        {
-          text: 'Отмена',
-          role: 'cancel',
-        },
-        {
-          text: 'Перевести',
-          handler: async () => {
-            try {
-              await this._makeTransaction(receiver, amount, description);
-              notifySuccess();
-            } catch (e) {
-              if (e && e.json && e.json() && e.json().message)
-                notifyAndReject(e.json().message);
-              else
-                notifyAndReject('Неизвестная ошибка сервера');
-            }
+        title: 'Подтверждение перевода',
+        message,
+        buttons: [
+          {
+            text: 'Отмена',
+            role: 'cancel',
           },
-        },
-      ],
+          {
+            text: 'Перевести',
+            handler: async () => {
+              try {
+                await this._makeTransaction(receiver, amount, description);
+                notifySuccess();
+              } catch (e) {
+                if (e && e.json && e.json() && e.json().message)
+                  notifyAndReject(e.json().message);
+                else
+                  notifyAndReject('Неизвестная ошибка сервера');
+              }
+            },
+          },
+        ],
+      });
     });
-  });
-}
+  }
 
   private _makeTransaction(receiver: string, amount: number, description: string) {
-  const requestBody = JSON.stringify({
-    sender: this._authService.getUserId(),
-    receiver,
-    amount,
-    description,
-  });
+    const requestBody = JSON.stringify({
+      sender: this._authService.getUserId(),
+      receiver,
+      amount,
+      description,
+    });
 
-  return this._http.post(GlobalConfig.economyTransferMoneyUrl, requestBody,
-    this._authService.getRequestOptionsWithSavedCredentials()).toPromise();
-}
+    return this._http.post(GlobalConfig.economyTransferMoneyUrl, requestBody,
+      this._authService.getRequestOptionsWithSavedCredentials()).toPromise();
+  }
 }
